8e8255da0fb9175f3ec2cbdcf8735e6eb2ad5b55
BigMcQueen <mmmrym10@gmail.com>
2023-03-11 20:44:04 +0900

    サインイン機能を実装

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def signup(request):
@@ -9,8 +10,21 @@ def signup(request):
         password = request.POST['password']
         try:
             user = User.objects.create_user(username, '', password)
-            return render(request, 'chat/signup.html', {'success': 'success'})
+            return render(request, 'chat/signup.html', {'context': 'success'})
         except IntegrityError:
-            return render(request, 'chat/signup.html', {'error': 'このユーザー名はすでに登録されています！'})
+            return render(request, 'chat/signup.html', {'context': 'このユーザー名はすでに登録されています！'})
     else:
-        return render(request, 'chat/signup.html', {'method': 'GET method'})
+        return render(request, 'chat/signup.html', {'context': 'GET method'})
+    
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'chat/signin.html', {'context': 'サインインしました'})
+        else:
+            return render(request, 'chat/signin.html', {'context': 'サインインできませんでした'})
+    else:
+        return render(request, 'chat/signin.html', {'context': 'GET method'})
