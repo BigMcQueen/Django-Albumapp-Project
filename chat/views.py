320f81718c56384c37ad28068b88e217b405caff
BigMcQueen <mmmrym10@gmail.com>
2023-03-11 20:30:48 +0900

    ユーザー名の重複を防ぐ機能を実装

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def signup(request):
-    return render(request, 'chat/signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'chat/signup.html', {'success': 'success'})
+        except IntegrityError:
+            return render(request, 'chat/signup.html', {'error': 'このユーザー名はすでに登録されています！'})
+    else:
+        return render(request, 'chat/signup.html', {'method': 'GET method'})
