3d6da1a06117e9baba6f8e9fc4149db47bd7d433
BigMcQueen <mmmrym10@gmail.com>
2023-03-12 02:44:33 +0900

    CreateViewを実装

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup_func, signin_func, listview_func, signout_func, detail_func, good_func
+from .views import signup_func, signin_func, listview_func, signout_func, detail_func, good_func, ChatCreate
 
 urlpatterns = [
     path('signup/', signup_func, name='signup'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signout/', signout_func, name='signout'),
     path('detail/<int:pk>', detail_func, name='detail'),
     path('good/<int:pk>', good_func, name='good'),
+    path('create/', ChatCreate.as_view(), name='create'),
 ]
