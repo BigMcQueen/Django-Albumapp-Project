2c0a03f1bee30afbf8eab395a12bb0912a7db5e5
BigMcQueen <mmmrym10@gmail.com>
2023-03-21 11:51:50 +0900

    View posted photos

--- album/views.py
+++ album/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
+
+from .models import Photo, Tag
 
 # Create your views here.
+class PhotoListView(ListView):
+    model = Photo
+    template_name = 'album/photo_list.html'
+    context_object_name = 'photos'
 
-def index(request):
-    return HttpResponse('Hello World!')
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = 'album/photo_detail.html'
+    context_object_name = 'photo'
+
+class TagPhotoListView(ListView):
+    model = Photo
+    template_name = 'album/tag_photo.html'
+    context_object_name = 'photos'
+
+    def get_queryset(self, **kwargs):
+        tag_name = self.kwargs['tag']
+        tag = get_object_or_404(Tag, name=tag_name)
+        return super().get_queryset().filter(tags=tag)
