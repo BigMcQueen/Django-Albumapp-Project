9da13ffb6f1506ecb57f93360a07bd15d4e72624
BigMcQueen <mmmrym10@gmail.com>
2023-03-21 21:58:31 +0900

    Indicate the selected tag

--- album/views.py
+++ album/views.py
@@ -29,3 +29,9 @@ class TagPhotoListView(ListView):
         tag_name = self.kwargs['tag']
         tag = get_object_or_404(Tag, name=tag_name)
         return super().get_queryset().filter(tags=tag)
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['tag'] = self.kwargs['tag']
+        return context
