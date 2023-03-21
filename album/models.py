ee43e7df58e0d82c51c5730e093b565b7acd1c55
BigMcQueen <mmmrym10@gmail.com>
2023-03-21 10:34:55 +0900

    Define a model for handling images

--- album/models.py
+++ album/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=32, primary_key=True)
+
+    def __str__(self) -> str:
+        return self.name
